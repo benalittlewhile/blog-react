81de3c1581c3a75c4c3489909cdf6b0d018f34fd
Ben <isieldor@gmail.com>
2020-11-21 20:37:57 -0600

    Updates

---
+++ precache-manifest.8187a410a6eed5fe53186109bb9c4fcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4937e23a7a97dea247af18a41d7071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b702ade3305cbe91df48",
+    "url": "/static/css/main.72bb4121.chunk.css"
+  },
+  {
+    "revision": "272602da8e616107e64d",
+    "url": "/static/js/2.47476a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47476a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b702ade3305cbe91df48",
+    "url": "/static/js/main.22c11407.chunk.js"
+  },
+  {
+    "revision": "34d3b6e295197e2cebdd",
+    "url": "/static/js/runtime-main.fa3222a7.js"
+  },
+  {
+    "revision": "d2bc9e861a3cbaf7f45abc6fdadec990",
+    "url": "/static/media/profile.d2bc9e86.jpeg"
+  }
+]);
