d54699670dd0f623b6f6e3c58d6a2c55f5a10394
Ben <isieldor@gmail.com>
2020-12-04 20:29:49 -0600

    added github logo and uses link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // need react to use react
 import React from "react";
+import GitHubLogo from "./GitHub-Mark-Light-64px.png";
 
 // import react-router-dom so I can use it to link different pages
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -20,6 +21,19 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/Blog">Blog</Link>
           <Link to="/ToLearn">tolearn</Link>
+          <a href="https://github.com/benalittlewhile/dotfiles">Config</a>
+          <div className="GHLinkLogo">
+            <a
+              className="GHLinkLogo"
+              href="https://github.com/benalittlewhile/"
+            >
+              <img
+                className="GHLinkLogo"
+                src={GitHubLogo}
+                alt="the Github logo"
+              ></img>
+            </a>
+          </div>
         </nav>
         <Switch>
           <Route exact path="/" component={Home}></Route>
